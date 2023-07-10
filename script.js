1d98e7e0dbde08609e6fc923d37cee0ad98a6bad
Amit Naskar <64121569+Amitn2003@users.noreply.github.com>
2023-07-10 22:08:18 +0530

    Update script.js

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ let month = today.getMonth() + 1;
 
 
 //User greetings with his/her own name...
-if (localStorage.getItem("name") == null || localStorage.getItem("name") == undefined || localStorage.getItem("name")  == "") {
+if (localStorage.getItem("name") == null || localStorage.getItem("name") == undefined || localStorage.getItem("name") == "") {
     let userName = prompt("Enter your name : ");
     localStorage.setItem("name", userName)
     console.log(userName + " From input now")
@@ -24,53 +24,59 @@ formHandler.addEventListener("submit", (e) => {
     e.preventDefault();
     let heading = ""
     if (e.target.heading.value == "") {
-        heading = `Another Day... ${date}/${month}/${year}`
+        // heading = `Another Day... ${date}/${month}/${year}`
+        heading = `Another Day... `
     }
     else {
         heading = e.target.heading.value
     }
     let message = e.target.message.value
-    let allData = JSON.parse(localStorage.getItem("diary") )?? [];
+    let allData = JSON.parse(localStorage.getItem("diary")) ?? [];
     let temp = {
         "date": date,
         "month": month,
         "year": year,
-        "heading" : heading,
-        "message" : message
+        "heading": heading,
+        "message": message
     }
     allData.push(temp)
-    localStorage.setItem("diary", JSON.stringify(allData) )
+    localStorage.setItem("diary", JSON.stringify(allData))
+    e.target.reset()
     showDiaryList()
-}) 
+})
 
 // to delete an element
 function dlt(i) {
-    let userDataL = JSON.parse(localStorage.getItem("diary")) ?? [] ;
-    userDataL.splice(i,1)
-    console.log(userDataL)
-    localStorage.setItem("diary", JSON.stringify(userDataL))
-    showDiaryList()
+    let confirmUser = confirm("Do you really want to delete " + (i + 1) + "th data?")
+    if (confirmUser == 1) {
+        let userDataL = JSON.parse(localStorage.getItem("diary")) ?? [];
+        userDataL.splice(i, 1)
+        console.log(userDataL)
+        localStorage.setItem("diary", JSON.stringify(userDataL))
+        showDiaryList();
+        // appendAlert('Deletation successful!!!!!', 'success')
+
+    }
 }
 
 // Show diary list
 function showDiaryList() {
-    let userDataList = JSON.parse(localStorage.getItem("diary")) ?? [] ;
+    let userDataList = JSON.parse(localStorage.getItem("diary")) ?? [];
     let doc = ""
-    for (let i = 0; i<userDataList.length; i++) {
+    for (let i = userDataList.length - 1; i >= 0; i--) {
         doc = doc + `
         <div class="diaryList">
             <h5>Date</h5>
             <span>${userDataList[i].date}/${userDataList[i].month}/${userDataList[i].year}</span>  
-            <span class="delete"  onclick="dlt(${i})"> <b>X</b>  Delete </span>
+            <span class="delete" data-bs-toggle="modal" data-bs-target="#exampleModal" onclick="dlt(${i})"> <b>X</b>  Delete </span>
             <h5>Heading</h5>
             <span>${userDataList[i].heading}</span>
             <h5>Message</h5>
             <span>${userDataList[i].message}</span>
-        </div> <br> `
+        </div> <br> <hr> `
     }
     let dryLst = document.getElementById("diary");
     dryLst.innerHTML = doc;
 }
 
 showDiaryList();
-
